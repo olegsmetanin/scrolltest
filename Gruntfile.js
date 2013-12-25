538f8724d84b4626481cf60da7dc354e407e0157
olegsmith <olegsmith@gmail.com>
2013-12-25 22:45:06 +0400

    move source to src folder

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,61 +5,71 @@
  */
 "use strict";
 
-module.exports = function(grunt) {
+module.exports = function (grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
+	// Project configuration.
+	grunt.initConfig({
 
-    // Project metadata
-    pkg   : grunt.file.readJSON('package.json'),
-    clean: {
-    	dist: ['dist/'],
-    },
+		// Project metadata
+		pkg: grunt.file.readJSON('package.json'),
+		clean: {
+			dist: ['dist/'],
+		},
 
-    assemble: {
-      options: {
-        flatten: false,
-        assets: 'dist',
-        data: 'data/*.{json,yml}',
-        // Templates
-        partials: 'partials/*.hbs',
-        layoutdir: 'layouts'
-      },
-      pages: {
-        src: ['*.hbs'],
-        dest: 'dist/'
-      },
-      modules: {
-        src: ['modules/**/*.hbs'],
-        dest: 'dist/'        
-      }    
-    },
-    copy: {
-    	vendor: {
-    		expand: true, src: ['vendor/**'], dest: 'dist/'
-    	} 
-    },
-    'gh-pages': {
-      options: {
-        base: 'dist'
-      },
-      src: ['**']
-    }
+		assemble: {
+			options: {
+				flatten: false,
+				assets: 'dist',
+				data: 'src/data/*.{json,yml}',
+				partials: 'src/partials/*.hbs',
+				layoutdir: 'src/layouts'
+			},
+			all: {
+				expand: true,
+				cwd: "src/",
+				src: ['*.hbs', 'modules/**/*.hbs'],
+				dest: 'dist/'
+			}
+		},
+		copy: {
+			all: {
+				expand: true,
+				cwd: "src/",
+				src: [
+					'*.*',
+					'modules/**',
+					'!**/*.hbs'],
+				dest: 'dist/'
+			},
+			vendor: {
+				expand: true,
+				cwd: "src/",
+				src: [
+					'vendor/**'
+				],
+				dest: 'dist/'
+			},
+		},
+		'gh-pages': {
+			options: {
+				base: 'dist'
+			},
+			src: ['**']
+		}
+	});
 
-  });
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('assemble');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-frep');
+	grunt.loadNpmTasks('grunt-sync-pkg');
+	grunt.loadNpmTasks('grunt-gh-pages');
 
-  // These plugins provide necessary tasks.
-  grunt.loadNpmTasks('assemble');
-  grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-frep');
-  grunt.loadNpmTasks('grunt-sync-pkg');
-  grunt.loadNpmTasks('grunt-gh-pages');
-
-  // Default task to be run with the "grunt" command.
-  grunt.registerTask('default', [
-  	'clean',
-  	'assemble',
-  	'copy'
-  ]);
+	// Default task to be run with the "grunt" command.
+	grunt.registerTask('default', [
+		'clean',
+		'assemble',
+		'copy'
+	]);
 };
